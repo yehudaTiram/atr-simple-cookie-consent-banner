9b8711a6a620c1ea31d539cd2d66e3cf77b2c6e0
Yehuda Tiram <yehuda@atarimtr.co.il>
2025-09-06 10:49:35 +0300

    1

--- admin/class-atr-simple-cookie-consent-banner-admin.php
+++ admin/class-atr-simple-cookie-consent-banner-admin.php
@@ -87,18 +87,6 @@ class ATR_Simple_Cookie_Consent_Banner_Admin {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in ATR_Simple_Cookie_Consent_Banner_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The ATR_Simple_Cookie_Consent_Banner_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/atr-simple-cookie-consent-banner-admin.css', array(), $this->version, 'all' );
 
 	}
@@ -109,19 +97,6 @@ class ATR_Simple_Cookie_Consent_Banner_Admin {
 	 * @since    2.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in ATR_Simple_Cookie_Consent_Banner_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The ATR_Simple_Cookie_Consent_Banner_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/atr-simple-cookie-consent-banner-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
